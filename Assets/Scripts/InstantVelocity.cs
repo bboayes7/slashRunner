1c708213d80e090b58215bd47b693241fa6f58ac
Matthew Broking <brokingm@gmail.com>
2018-09-10 23:34:31 -0700

    Modular Camera, Paralax Scrolling, Spawner

---
+++ Assets/Scripts/InstantVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantVelocity : MonoBehaviour {
+
+    public Vector2 velocity = Vector2.zero;
+
+    private Rigidbody2D body2d;
+
+    private void Awake() {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        body2d.velocity = velocity;
+    }
+}
